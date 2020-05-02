ec5f9000d372acf0d21c5a0506d484ed8b6593ec
Kyle Ordona <janzrel.ordona@obf.ateneo.edu>
2020-05-02 23:10:23 +0800

    added table for dashboard

--- imports/ui/components/CandidatesTally.jsx
+++ imports/ui/components/CandidatesTally.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 const CandidatesTally = ({ tallies }) => {
+  const totalVotes = tallies.reduce((total, curr) => total + curr.votes, 0);
+
   return (
-    <div>
-      {tallies.map((tally) => (
-        <div key={tally._id}>
-          {tally.name}: {tally.votes}
-        </div>
+    <TableBody>
+      {tallies.map((row) => (
+        <TableRow key={row._id}>
+          <TableCell component='th' scope='row'>
+            {row.name}
+          </TableCell>
+          <TableCell align='right'>{row.votes}</TableCell>
+          <TableCell align='right'>
+            {Math.round((row.votes / totalVotes) * 10000) / 100}%
+          </TableCell>
+        </TableRow>
       ))}
-    </div>
+    </TableBody>
   );
 };
 
--- imports/ui/pages/Dashboard.jsx
+++ imports/ui/pages/Dashboard.jsx
@@ -1,8 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import React, { useState } from 'react';
+import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 
 import { makeStyles, Grid, Typography } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 import { Voted } from '../../api/voted';
 import { Votes } from '../../api/votes';
@@ -18,8 +23,14 @@ import {
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(4, 2),
-    flexDirection: 'column',
-    backgroundColor: theme.palette.primary.light
+    // flexDirection: 'column',
+    justifyContent: 'center',
+    // alignContent: 'center',
+    backgroundColor: theme.palette.primary.light,
+  },
+  paper: {
+    padding: theme.spacing(3,3),
+    margin: theme.spacing(2,0)
   },
 }));
 
@@ -60,17 +71,34 @@ const Dashboard = () => {
           Dashboard
         </Typography>
       </Grid>
-      <Grid item sm={12}>
+      <Grid item sm={10}>
         <Typography component='h3' variant='h5'>
           Total votes casted: {totalVotes}
         </Typography>
       </Grid>
       {positionTallies.map((tallies) => (
+        <Grid item sm={10} key={tallies._id}>
+          <Paper className={classes.paper}>
+            <Typography variant='h6'>{tallies.position}</Typography>
+            <Table aria-label='simple table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell align='right'>Votes</TableCell>
+                  <TableCell align='right'>Percentage</TableCell>
+                </TableRow>
+              </TableHead>
+              <CandidatesTally tallies={tallies.candidates}></CandidatesTally>
+            </Table>
+          </Paper>
+        </Grid>
+      ))}
+      {/* {positionTallies.map((tallies) => (
         <div key={tallies._id}>
           <h4>{tallies.position}</h4>
           <CandidatesTally tallies={tallies.candidates}></CandidatesTally>
         </div>
-      ))}
+      ))} */}
     </Grid>
   ) : (
     <Loader />
