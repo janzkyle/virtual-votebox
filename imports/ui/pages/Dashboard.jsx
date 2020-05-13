d8f5d2c6347b4829ac4b12d46590541378378fae
Kyle Ordona <janzrel.ordona@obf.ateneo.edu>
2020-05-13 23:29:31 +0800

    added back the tallies in dashboad

--- imports/ui/pages/Dashboard.jsx
+++ imports/ui/pages/Dashboard.jsx
@@ -3,15 +3,28 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 
 import { makeStyles, Grid, Typography } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 import { Voted } from '../../api/voted';
+import { Votes } from '../../api/votes';
+import { Positions } from '../../api/positions';
 
 import Loader from '../components/Loader';
+import CandidatesTally from '../components/CandidatesTally';
+import {
+  groupCandidates,
+  insertCandidatesToPositions,
+} from '../../util/helper';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(4, 2),
     justifyContent: 'center',
+    backgroundColor: theme.palette.primary.light,
   },
   paper: {
     margin: theme.spacing(2, 0),
@@ -34,16 +47,36 @@ const useStyles = makeStyles((theme) => ({
 
 const useVoteCount = () =>
   useTracker(() => {
-    const subscription = Meteor.subscribe('voted');
+    Meteor.subscribe('voted');
     const totalVotes = Voted.find().count();
-    return { totalVotes, totalVotesLoaded: subscription.ready() };
+    const voted = Voted.find({ userId: Meteor.userId() }).fetch();
+    return { totalVotes, hasVoted: !!voted.length };
+  }, []);
+
+const useTallies = () =>
+  useTracker(() => {
+    const subscription = Meteor.subscribe('tallies');
+    const candidates = Votes.find().fetch();
+    return { candidates, candidatesLoaded: subscription.ready() };
+  }, []);
+
+const usePositions = () =>
+  useTracker(() => {
+    const subscription = Meteor.subscribe('positions');
+    const positions = Positions.find().fetch();
+    return { positions, positionsLoaded: subscription.ready() };
   }, []);
 
 const Dashboard = () => {
   const classes = useStyles();
-  const { totalVotes, totalVotesLoaded } = useVoteCount();
+  const { totalVotes, hasVoted } = useVoteCount();
+  const { candidates, candidatesLoaded } = useTallies();
+  const { positions, positionsLoaded } = usePositions();
 
-  return totalVotesLoaded ? (
+  const grouped = groupCandidates(candidates, 'position');
+  const positionTallies = insertCandidatesToPositions(positions, grouped);
+
+  return candidatesLoaded && positionsLoaded ? (
     <Grid container className={classes.root}>
       <Grid item sm={12}>
         <Typography align='center' component='h2' variant='h4'>
@@ -55,9 +88,42 @@ const Dashboard = () => {
           Total votes casted: {totalVotes}
         </Typography>
       </Grid>
-      <Grid item sm={7}>
-        <Typography>Summary of votes is not yet available</Typography>
-      </Grid>
+      {hasVoted ? (
+        positionTallies.map((tallies) => (
+          <Grid item sm={7} key={tallies._id}>
+            <Paper className={classes.paper}>
+              <Typography variant='h6' className={classes.position}>
+                {tallies.position}
+              </Typography>
+              <Table aria-label='simple table'>
+                <colgroup>
+                  <col style={{ width: '40%' }} />
+                  <col style={{ width: '30%' }} />
+                  <col style={{ width: '30%' }} />
+                </colgroup>
+                <TableHead>
+                  <TableRow>
+                    <TableCell className={classes.tableHeader}>
+                      Candidate
+                    </TableCell>
+                    <TableCell align='right' className={classes.tableHeader}>
+                      Votes
+                    </TableCell>
+                    <TableCell align='right' className={classes.tableHeader}>
+                      Percentage
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <CandidatesTally tallies={tallies.candidates}></CandidatesTally>
+              </Table>
+            </Paper>
+          </Grid>
+        ))
+      ) : (
+        <Grid item sm={7}>
+          <Typography>Summary of votes is not yet available</Typography>
+        </Grid>
+      )}
     </Grid>
   ) : (
     <Loader />
